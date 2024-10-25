ba12d2b1400032649dcce943ae2f66975480c161
seregazakharin <seregaz242004@gmail.com>
2024-10-25 22:37:10 +0300

    Update automaton.h
    добавлены методы для клонирования и повторения автоматов, увеличен список include

--- automaton.h
+++ automaton.h
@@ -4,9 +4,20 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <cstdlib>
+#include <ctime>
+#include <set>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <stack>
+#include <algorithm>
+#include <queue>
+#include <utility>
+#include <functional>
+#include <memory>
+
+enum LexemeType { EOL, ATOM, LBR, RBR, DOT };
 
 // Класс для представления состояния автомата
 class State {
@@ -20,7 +31,7 @@ public:
     void print() const {
         std::cout << "State " << name << " transitions:\n";
         for (const auto& transition : transitions) {
-            std::cout << "  " << name << " -- " << transition.first << " --> " << transition.second << "\n";
+            std::cout << "  " << name << " -- \"" << transition.first << "\" --> " << transition.second << "\n";
         }
     }
 };
@@ -32,13 +43,40 @@ public:
     int start;  // Начальное состояние
     std::unordered_set<int> finals;  // Финальные состояния
     std::vector<int> alphabet;  // Алфавит автомата
+    LexemeType type;  // Тип лексемы
 
     Automaton(const std::vector<State>& states, const int& start, 
-              const std::unordered_set<int>& finals, const std::vector<int>& alphabet)
-        : states(states), start(start), finals(finals), alphabet(alphabet) {}
+              const std::unordered_set<int>& finals, const std::vector<int>& alphabet, LexemeType type)
+        : states(states), start(start), finals(finals), alphabet(alphabet), type(type) {}
+
+    Automaton() {
+    }
+
+    // Метод для клонирования автомата
+    Automaton clone() const {
+        return Automaton(states, start, finals, alphabet, type);
+    }
+
+    // Метод для повторения автомата
+    Automaton repeat(int minRepeats = 0) const {
+        Automaton repeated_automaton = clone();
+
+        for (int final_state : finals) {
+            repeated_automaton.states[final_state].transitions[-1] = start;
+        }
+
+        if (minRepeats == 0) {
+            int new_start = repeated_automaton.states.size();
+            repeated_automaton.states.push_back(State(new_start, {{-1, start}}));
+            return Automaton(repeated_automaton.states, new_start, finals, alphabet, type);
+        }
+
+        return repeated_automaton;
+    }
 
     void print() const {
-        std::cout << "start state: " << start << "\n";
+        std::cout << "Lexeme type: " << type << "\n";
+        std::cout << "Start state: " << start << "\n";
         std::cout << "Final states: ";
         for (const auto& fin : finals) {
             std::cout << fin << " ";
