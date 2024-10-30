a9bc48be8abfc5414ba679804b5d6b7b97aeefc7
milena <milenautebaeva@gmail.com>
2024-10-30 12:56:33 +0300

    little fixex

--- table.cpp
+++ table.cpp
@@ -9,146 +9,18 @@ using namespace std;
 #define endl '\n'
 
 
-map<int, string> S; // основная часть таблицы
-map<int, string> E; // столбцы таблицы = суффиксы
-map<pair<int, int>, int> table; // таблица принадлежностей 
-map<int, int> is_main; // хранит флаг принадлежности к основной части, 0 - доп, 1 - основная
+map<int, string> S; // строки таблицы = классы эквивалентности
+map<int, string> E; // столбцы таблицы = различающие суффиксы 
+map<pair<int, int>, int> table; // таблица входимости
+map<int, int> is_main; // хранит флаг принадлежности к основной части, 1 - основная часть, 0 - доп часть
 
-vector<string> alpha = {"0", "1"};
+vector<string> alpha = {"0", "1"}; // алфавит языка
 
-set<string> statuses;
-set<string> in;
-int poln = 0;
-int neprot = 0;
+set<string> in; // набор всех слов-классов
+int poln = 0; // является ли таблица полной
+int neprot = 0; // является ли таблица непротиворечивой
 
-int check(string w){
-    // делаем запрос мату
-    return 1;
-}
-
-string equivalence(){
-    // проверка на эквивалентность
-    string status = "help";  // снова тест, но help это прям мне нужна!!!
-    vector<string> to_mat;
-    for (auto i: S){
-        string word = i.second;
-        to_mat.push_back(word);
-    }
-    // cout << "we are push it to mat!!!" << endl;
-    for (string i: to_mat){
-        cout << i << endl;
-    }
-
-    return status;
-}
-
-string concat(string a, string b){
-    if (a == "e"){
-        return b;
-    }
-    if (b == "e"){
-        return a;
-    }
-    return a + b;
-}
-
-void proverka(){
-    for (int i = 0; i < S.size(); ++i){
-        for (int j = 0; j < E.size(); ++j){
-            string new_str = concat(S[i], E[j]);
-            // cout << "s: " << S[i] << " E " << E[j] << " new str " << new_str << ' ';
-            int f = check(new_str); // проверка принадлежности слова
-            table[{i, j}] = f; // записали принадлежит ли слово S[i] + E[j] языку
-        }
-    }
-}
-
-void build(){
-    // строим дополнительную часть
-    int n = S.size(); // тк в процесса изменяется
-    for (int i = 0; i < n; ++i){
-        for (int j = 0; j < alpha.size(); ++j){
-            if (is_main[i] != 1){
-                continue; // если слово не в главной части - скип
-            }
-            string new_str = concat(S[i], alpha[j]);
-            // cout << "s: " << S[i] << " alpha " << alpha[j] << " new str " << new_str << ' ';
-            if (in.find(new_str) != in.end()){ // если мы получали уже это слово
-                continue;
-            }
-            // cout << "in " << S.size() << " add " << new_str;
-            S[S.size()] = new_str; // получили новое слово путём конкатенации
-            is_main[S.size()] = 0; // нужно, тк в мапе не было до этого значений
-            in.insert(new_str); // тк сет, то пофиг, но это действительно новое слово
-            // cout << endl;
-        }
-    }
-}
-
-string get_status(int ind){
-    string row = "";
-    for (int j = 0; j < E.size(); ++j){
-        row = row + to_string(table[{ind, j}]); // копим статусы по строке
-    }
-    return row;
-}
-
-int polnota(){
-    int is_poln = 1; // полна ли таблица на этой итерации
-    set<string> pol; // строки-статусы, которые отражают принадлежность
-    for (int i = 0; i < S.size(); ++i){
-        string row = ""; // строка со статусами
-        for (int j = 0; j < E.size(); ++j){// надо бы заменить на функцию
-            row = row + to_string(table[{i, j}]); // накапливаем статусы строки 
-        }
-        // string row2 = get_status(i);
-        // cout << "NORM???? " << row << ' ' << row2 << endl;
-        if (is_main[i] == 1){ // если это основная часть, то чекать не нужно
-            // cout << "not main: " << i << endl;
-            pol.insert(row);
-        }  
-        else{
-            // row = "0"; // test again
-            if (pol.find(row) == pol.end()){ // если в доп части есть новая строка - сохраняем
-                is_poln = 0; // обновили таблицу => она не была полной
-                // cout << "add in main: " << i << endl;
-                pol.insert(row);
-                is_main[i] = 1; // переносим её в основную
-            }
-        }
-    }
-    return is_poln;
-}
-
-int neprotivor(){
-    int is_neprot = 1;
-    for (int i = 0; i < S.size(); ++i){
-        if (!is_main[i]){
-            continue; // cкипаем слово, если оно не в основной части
-        }
-        for (int j = i + i; j < S.size(); ++j){
-            if (!is_main[j]){
-                continue; // cкипаем слово, если оно не в основной части
-            }
-            string status_1 = get_status(i);
-            string status_2 = get_status(j);
-            if (status_1 == status_2){ // если строчки одинаковые, то есть на всех суффиксах эти префиксы ведут себя одинаково
-                for (int k = 0; k < E.size(); ++k){
-                    for (string al: alpha){
-                        int f1 = check((S[i] + al + E[k]));
-                        int f2 = check((S[j] + al + E[k]));
-                        if (f1 != f2){
-                            add_to_suf((al + E[k]));
-                            is_neprot = 0;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return is_neprot;
-}
-
+// вывод основных сущностей
 void print(){
     cout << "pref: " << endl;
     for (int i = 0; i < S.size(); ++i){
@@ -178,23 +50,154 @@ void print(){
     }
 }
 
+// запрос к мату на принадлежность
+int check(string w){
+    return 1;
+}
+
+// запрос к мату на эквивалентность
+string equivalence(){
+    string status = "ok";
+    status = "help";  // снова тест, но help это прям мне нужна!!!
+    vector<string> to_mat; // классы эквивалентности, которые отправляем мату 
+    for (auto i: S){
+        string word = i.second;
+        to_mat.push_back(word);
+    }
+    // cout << "we are push it to mat!!!" << endl;
+    // for (string i: to_mat){
+    //     cout << i << endl;
+    // }
+    return status;
+}
+
+// учитываем пустую строчку при конкатенации
+string concat(string a, string b){
+    if (a == "e"){
+        return b;
+    }
+    if (b == "e"){
+        return a;
+    }
+    return a + b;
+}
+
+// добавляем слово и его суффиксы в качестве столбцов
+void add_to_suf(string w){
+    string w_rev = ""; //чтобы не было O(n^2)
+    for (int i = w.size() - 1; i >= 0; --i){
+        w_rev = concat(to_string(w[i]), w_rev);
+        E[E.size()] = w_rev;
+    }
+}
+
+// строка со статусами принадлежности
+string get_status(int ind){
+    string row = "";
+    for (int j = 0; j < E.size(); ++j){
+        row = row + to_string(table[{ind, j}]); 
+    }
+    return row;
+}
+
+// заполнение таблицы принадлежности
+void proverka(){
+    for (int i = 0; i < S.size(); ++i){
+        for (int j = 0; j < E.size(); ++j){
+            string new_str = concat(S[i], E[j]);
+            // cout << "s: " << S[i] << " E " << E[j] << " new str " << new_str << ' ';
+            int f = check(new_str); // проверка принадлежности слова
+            table[{i, j}] = f; // записали принадлежит ли слово S[i] + E[j] языку
+        }
+    }
+}
+
+// строим дополнительную часть
+void build(){
+    int n = S.size(); // тк в процесса изменяется
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < alpha.size(); ++j){
+            if (is_main[i] != 1){
+                continue; // если слово не в главной части - скип
+            }
+            string new_str = concat(S[i], alpha[j]);
+            // cout << "s: " << S[i] << " alpha " << alpha[j] << " new str " << new_str << ' ';
+            if (in.find(new_str) != in.end()){ // если мы получали уже это слово
+                continue;
+            }
+            // cout << "in " << S.size() << " add " << new_str;
+            S[S.size()] = new_str; // получили новое слово путём конкатенации
+            is_main[S.size()] = 0; // нужно, тк в мапе не было до этого значений
+            in.insert(new_str); // тк сет, то пофиг, но это действительно новое слово
+            // cout << endl;
+        }
+    }
+}
+
+// проверка таблицы на полноту
+int polnota(){
+    int is_poln = 1; // полна ли таблица на этой итерации
+    set<string> pol; // строки-статусы, которые отражают принадлежность
+    for (int i = 0; i < S.size(); ++i){
+        string row = get_status(i);
+        if (is_main[i] == 1){ // если это основная часть, то чекать не нужно
+            // cout << "not main: " << i << endl;
+            pol.insert(row);
+        }  
+        else{
+            // row = "0"; // test again
+            if (pol.find(row) == pol.end()){ // если в доп части есть новая строка - сохраняем
+                is_poln = 0; // обновили таблицу => она не была полной
+                // cout << "add in main: " << i << endl;
+                pol.insert(row);
+                is_main[i] = 1; // переносим её в основную
+            }
+        }
+    }
+    return is_poln;
+}
+
+// проверка на непротиворечивость
+int neprotivor(){
+    int is_neprot = 1;
+    for (int i = 0; i < S.size(); ++i){
+        if (!is_main[i]){
+            continue; // cкипаем слово, если оно не в основной части
+        }
+        for (int j = i + 1; j < S.size(); ++j){
+            if (!is_main[j]){
+                continue; // cкипаем слово, если оно не в основной части
+            }
+            string status_1 = get_status(i);
+            string status_2 = get_status(j);
+            if (status_1 == status_2){ // если строчки одинаковые, то есть на всех суффиксах эти префиксы ведут себя одинаково
+                for (int k = 0; k < E.size(); ++k){
+                    for (string al: alpha){
+                        int f1 = check((S[i] + al + E[k]));
+                        int f2 = check((S[j] + al + E[k]));
+                        // проблемс
+                        if (f1 != f2){
+                            add_to_suf((al + E[k]));
+                            is_neprot = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return is_neprot;
+}
+
+// доведение таблицы до состояния, когда можно делать запрос мату
 void fill_table(){
     while (!neprot){
         while (!poln){
-            build(); // достариваем таблицу
+            build(); // достариваем таблицу, всё ок, тк при исполнении polnota() мы добавили новую строку
             proverka(); // проверяем каждый статус
             poln = polnota(); // проверяем на полноту
             print();
         }
-        neprot = neprotivor();
-    }
-}
-
-void add_to_suf(string w){
-    string w_rev = ""; //чтобы не было O(n^2)
-    for (int i = w.size() - 1; i >= 0; --i){
-        w_rev = w[i] + w_rev;
-        E[E.size()] = w_rev;
+        neprot = neprotivor(); // проверка на противоречивость
     }
 }
 
@@ -206,14 +209,8 @@ int main(){
     in.insert("e");
     is_main[0] = 1; // сразу ставим 1, тк изначально что-то говорим про пустое слово
 
-    // как делаем?
-    // строим дополнительную часть
-    // проверяем каждое слово
-    // проверяем на полноту, то есть нужно ли добавить ещё строки
-    // если полнота = 1, то делаем запрос к мату, иначе всё по новой
     while (!win){
         fill_table();
-
         string w = equivalence();
         if (w == "ok"){
             win = 1;
@@ -224,7 +221,6 @@ int main(){
             win = 1; // снова заглушка
         }
     }
-
 
     return 0;
 }
